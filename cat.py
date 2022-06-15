2ff26e130ab73ef394572f393bd9d7c0cc704925
NatalieSerra <natalie@pop-os.localdomain>
2022-06-16 00:35:37 +0300

    initial commit

--- cat.py
+++ cat.py
@@ -1,37 +1,74 @@
-import os
+from crypt import methods
 import catboys as catboy
+import nekos as catgirl
+import os
 import requests
 from pathlib import Path
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
-FOLDER = os.path.join('static', 'cat_boys')
+FOLDER = os.path.join('static', 'cats')
 
-url = catboy.img()
-img_data = requests.get(url)
-open("catboy.jpg", "wb").write(img_data.content)
-Path("catboy.jpg").rename("./static/cat_boys/catboy.jpg")
+catboy_url = catboy.img()
+catboy_data = requests.get(catboy_url)
+open("catboy.jpg", "wb").write(catboy_data.content)
+Path("catboy.jpg").rename("./static/cats/catboy.jpg")
+
+catgirl_url = catgirl.img("neko")
+cargirl_data = requests.get(catgirl_url)
+open("catgirl.jpg", "wb").write(cargirl_data.content)
+Path("catgirl.jpg").rename("./static/cats/catgirl.jpg")
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = FOLDER
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'catboy.jpg')
+
+@app.errorhandler(404)
+def bad_request(e):
+    return redirect(url_for('error'))
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+
+@app.route('/')
+def main():
+    return redirect(url_for('catboys'))
+
+
+@app.route('/catboys', methods=['GET', 'POST'])
+def catboys():
+    full_catboy_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'catboy.jpg')
     print(request.method)
 
     if request.method == 'POST':
-        if request.form.get('NewCat') == 'NewCat':
-            url = catboy.img()
-            print(url)
-            img_data = requests.get(url)
-            open("catboy.jpg", "wb").write(img_data.content)
-            Path("catboy.jpg").rename("./static/cat_boys/catboy.jpg")
+        if request.form.get('NewCat') == 'More CatBoys!':
+            catboy_url = catboy.img()
+            catboy_data = requests.get(catboy_url)
+            open("catboy.jpg", "wb").write(catboy_data.content)
+            Path("catboy.jpg").rename("./static/cats/catboy.jpg")
         else:
-            return "no"
-    elif request.method == 'GET':
-        print(" ")
+            return render_template("error.html")
 
-    return render_template("index.html", owo_image=full_filename)
+    return render_template("catboys.html", catboy_image=full_catboy_filename)
+
+
+@app.route('/catgirls', methods=['GET', 'POST'])
+def catgirls():
+    full_catgirl_filename = os.path.join(app.config['UPLOAD_FOLDER'], "catgirl.jpg")
+
+    if request.method == 'POST':
+        if request.form.get('NewCatG') == 'More CatGirls!':
+            catgirl_url = catgirl.img("neko")
+            cargirl_data = requests.get(catgirl_url)
+            open("catgirl.jpg", "wb").write(cargirl_data.content)
+            Path("catgirl.jpg").rename("./static/cats/catgirl.jpg")
+        else:
+            return render_template("error.html")
+
+    return render_template("catgirls.html", catgirl_image=full_catgirl_filename)
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
